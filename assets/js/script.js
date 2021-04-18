bd71c2a7fcd4f88c7a875969fa64b4946b65ae98
Brad Smith <smithfamily42@gmail.com>
2021-04-18 15:47:53 -0700

    added timer to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 //custom js for JSQuiz
+var timerEl = document.getElementById("countdown");
+var startBtn = document.getElementById("start");
+
+
+function countdown() {
+    //sets the starting time
+    var timeLeft = 80;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Out of Time";
+            startBtn.textContent = "Restart Game";
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+startBtn.onclick = countdown;
